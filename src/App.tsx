c1f3afd1215776762115cd56c4bef83762866fcc
JaredThacker <pricesmayvary302@gmail.com>
2024-09-25 16:59:04 -0400

    box is draggable

--- src/App.tsx
+++ src/App.tsx
@@ -7,35 +7,57 @@ function App() {
   const containerRef = useRef<HTMLDivElement>(null);
   const isClicked = useRef<Boolean>(false);
 
+  const coords = useRef<{
+    startX: number;
+    startY: number;
+    lastX: number;
+    lastY: number;
+  }>({
+    startX: 0,
+    startY: 0,
+    lastX: 0,
+    lastY: 0,
+  });
+
   useEffect(() => {
     if (!boxRef.current || !containerRef.current) return;
 
     const box = boxRef.current;
     const container = containerRef.current;
 
-    const onMouseDown = () => {
+    const onMouseDown = (event: MouseEvent) => {
       isClicked.current = true;
+      coords.current.startX = event.clientX;
+      coords.current.startY = event.clientY;
     };
 
-    const onMouseUp = () => {
+    const onMouseUp = (event: MouseEvent) => {
       isClicked.current = true;
+      coords.current.lastX = box.offsetLeft;
+      coords.current.lastY = box.offsetTop;
     };
 
     const onMouseMove = (event: MouseEvent) => {
       if (!isClicked.current) return;
 
-      box.style.top = `${event.clientY}px`;
-      box.style.left = `${event.clientX}px`;
+      const nextX =
+        event.clientX - coords.current.startX + coords.current.lastX;
+      const nextY =
+        event.clientY - coords.current.startY + coords.current.lastY;
+      box.style.left = `${nextX}px`;
+      box.style.top = `${nextY}px`;
     };
 
     box.addEventListener("mousedown", onMouseDown);
     box.addEventListener("mouseup", onMouseUp);
     container.addEventListener("mousemove", onMouseMove);
+    container.addEventListener("mouseleave", onMouseUp);
 
     const cleanup = () => {
       box.removeEventListener("mousedown", onMouseDown);
       box.removeEventListener("mouseup", onMouseUp);
       container.removeEventListener("mousemove", onMouseMove);
+      container.removeEventListener("mouseleave", onMouseUp);
     };
 
     return cleanup;
