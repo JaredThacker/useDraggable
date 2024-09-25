e65f324be051b32d39e6f07e0116c4a4a4094965
JaredThacker <pricesmayvary302@gmail.com>
2024-09-25 16:50:32 -0400

    added event listeners for mouse down, up, and move

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,41 @@ import "./App.css";
 function App() {
   const boxRef = useRef<HTMLDivElement>(null);
   const containerRef = useRef<HTMLDivElement>(null);
+  const isClicked = useRef<Boolean>(false);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    if (!boxRef.current || !containerRef.current) return;
+
+    const box = boxRef.current;
+    const container = containerRef.current;
+
+    const onMouseDown = () => {
+      isClicked.current = true;
+    };
+
+    const onMouseUp = () => {
+      isClicked.current = true;
+    };
+
+    const onMouseMove = (event: MouseEvent) => {
+      if (!isClicked.current) return;
+
+      box.style.top = `${event.clientY}px`;
+      box.style.left = `${event.clientX}px`;
+    };
+
+    box.addEventListener("mousedown", onMouseDown);
+    box.addEventListener("mouseup", onMouseUp);
+    container.addEventListener("mousemove", onMouseMove);
+
+    const cleanup = () => {
+      box.removeEventListener("mousedown", onMouseDown);
+      box.removeEventListener("mouseup", onMouseUp);
+      container.removeEventListener("mousemove", onMouseMove);
+    };
+
+    return cleanup;
+  }, []);
   return (
     <main>
       <div ref={containerRef} className="container">
